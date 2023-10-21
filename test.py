c9ea98d652e2428ce3bb56f727e353ac9c5a159f
weixing-007 <51946274+weixing-007@users.noreply.github.com>
2023-10-21 15:09:28 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# driver = webdriver.Chrome()
+# driver.get("https://www.baidu.com")
+# int = (By.ID, "kw")
+# driver.find_element(*int).send_keys("hello word")
+
+print("Hello Word")
